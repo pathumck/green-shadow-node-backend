d5ba836e0270c4cbb875b7fca88a2196cf954df3
pathumck <ckpathumck@gmail.com>
2025-03-28 22:47:33 +0530

    remove inplace ts compiling

--- src/routes/log_router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const log_controller_1 = require("../controller/log_controller");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const logRouter = (0, express_1.Router)();
-logRouter.post("/", authMiddleware_1.authenticateUser, (0, authMiddleware_1.authorizeRole)("MANAGER", "ADMIN", "SCIENTIST"), log_controller_1.createLog);
-logRouter.get("/", authMiddleware_1.authenticateUser, (0, authMiddleware_1.authorizeRole)("MANAGER", "ADMIN", "SCIENTIST"), log_controller_1.getAllLogs);
-exports.default = logRouter;
